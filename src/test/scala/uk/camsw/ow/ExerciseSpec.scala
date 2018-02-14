7f6d34ac4872b0fbd16565bdeaec20fe9a823b58
Leon Jones <leonj_24@yahoo.com>
2018-02-14 09:21:55 +0000

    Add failing test for population count

--- src/test/scala/uk/camsw/ow/ExerciseSpec.scala
+++ src/test/scala/uk/camsw/ow/ExerciseSpec.scala
@@ -1,15 +1,27 @@
 package uk.camsw.ow
 
+import org.scalacheck.Gen
 import org.scalatest.FreeSpec
 import uk.camsw.ow.Exercise.populationCount
 import org.scalatest.Matchers._
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 class ExerciseSpec extends FreeSpec {
+
+  private val positiveNumbers = Gen.choose(1, Int.MaxValue)
 
   "The population count function" - {
 
     "must return zero when applied to zero" in {
       populationCount(0) shouldBe 0
+    }
+
+
+    "must return a count greater than or equal to one for any value > 0" in {
+      GeneratorDrivenPropertyChecks.forAll(positiveNumbers){n => {
+        populationCount(n) should be >= 1
+      }}
+
     }
   }
 
