0052c5d697ff2bc19ad28385d5e78d9dfa96dc11
Leon Jones <leonj_24@yahoo.com>
2018-02-14 11:32:22 +0000

    Implement roundup pow 2 func

--- src/main/scala/uk/camsw/ow/Exercise.scala
+++ src/main/scala/uk/camsw/ow/Exercise.scala
@@ -6,10 +6,16 @@ object Exercise {
 
   val isPow2: Int => Boolean = n => n != 0 && ((n & -n) == n)
 
-  @tailrec
-  def roundUpPow2(n: Int, acc: Int = 0): Int =
+  def roundUpPow2(n: Int): Int = {
+
+    @tailrec
+    def go(x: Int, acc: Int = 0): Int =
+      if (x == 0) 1 << acc
+      else go(x >> 1, acc + 1)
+
     if (isPow2(n)) n
-    else roundUpPow2(n >> 1, acc + 1)
+    else go(n)
+  }
 
 
   def populationCount(n: Int): Int = {
--- src/test/scala/uk/camsw/ow/SupportFunctionSpec.scala
+++ src/test/scala/uk/camsw/ow/SupportFunctionSpec.scala
@@ -4,8 +4,7 @@ import org.scalacheck.Gen
 import org.scalatest.FreeSpec
 import org.scalatest.prop.GeneratorDrivenPropertyChecks._
 import org.scalatest.Matchers._
-import uk.camsw.ow.Exercise.isPow2
-
+import uk.camsw.ow.Exercise.{isPow2, roundUpPow2}
 
 class SupportFunctionSpec extends FreeSpec {
 
@@ -23,18 +22,50 @@ class SupportFunctionSpec extends FreeSpec {
     }
 
     "must return false for non powers of two" in {
-      forAll(powersOf2)(n =>
-        whenever(n > 1) {
-          isPow2(n + 1) shouldBe false
-        }
+      forAll(nonPowersOf2)(n =>
+        isPow2(n) shouldBe false
       )
+    }
+  }
+
+  "roundUpPow2" - {
+    "must return the given value for any input which is a power of two" in {
+      forAll(powersOf2)(n =>
+        roundUpPow2(n) shouldBe n
+      )
+    }
+
+    "must return a value that is a power of two" in {
+      forAll(anyIntInRange)(n =>
+        isPow2(roundUpPow2(n)) shouldBe true
+      )
+    }
+
+    "must return something greater than or equal to the given value" in {
+      forAll(anyIntInRange)(n =>
+        roundUpPow2(n) should be >= n
+      )
+    }
+
+    "must round up zero to 1" in {
+      roundUpPow2(0) shouldBe 1
+    }
+
+    "must round up specific examples correctly" in {
+      roundUpPow2(0) shouldBe 1
+      roundUpPow2(1) shouldBe 1
+      roundUpPow2(2) shouldBe 2
+      roundUpPow2(3) shouldBe 4
+      roundUpPow2(5) shouldBe 8
     }
   }
 }
 
 
 object SupportFunctionSpec {
+  val anyIntInRange: Gen[Int] = Gen.choose(0, 100000)
   val powersOf2: Gen[Int] = Gen.choose(0, 20).map(1 << _)
+  val nonPowersOf2: Gen[Int] = Gen.choose(2, 20).map(n => (1 << n) + 1)
 
 
 }
