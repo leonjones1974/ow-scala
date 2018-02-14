e8ce42400227e4540b4f2dc19775dc58c5dadf1c
Leon Jones <leonj_24@yahoo.com>
2018-02-14 10:11:57 +0000

    Add failing tests

--- src/test/scala/uk/camsw/ow/ExerciseSpec.scala
+++ src/test/scala/uk/camsw/ow/ExerciseSpec.scala
@@ -4,11 +4,16 @@ import org.scalacheck.Gen
 import org.scalatest.FreeSpec
 import uk.camsw.ow.Exercise.populationCount
 import org.scalatest.Matchers._
+import org.scalatest.matchers.{BeMatcher, MatchResult}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 class ExerciseSpec extends FreeSpec {
 
-  private val positiveNumbers = Gen.choose(1, Int.MaxValue)
+  import ExerciseSpec._
+
+  private val numbersGreaterThanZero = Gen.choose(1, Int.MaxValue)
+  private val oddNumbersGreaterThanOne =
+    numbersGreaterThanZero.filter(n => n > 1 && isOdd(n))
 
   "The population count function" - {
 
@@ -16,13 +21,49 @@ class ExerciseSpec extends FreeSpec {
       populationCount(0) shouldBe 0
     }
 
-
     "must return a count greater than or equal to one for any value > 0" in {
-      GeneratorDrivenPropertyChecks.forAll(positiveNumbers){n => {
-        populationCount(n) should be >= 1
-      }}
+      GeneratorDrivenPropertyChecks.forAll(numbersGreaterThanZero)(
+        populationCount(_) should be >= 1
+      )
+    }
 
+    "must return an even number for any odd number > 1" in {
+      GeneratorDrivenPropertyChecks.forAll(oddNumbersGreaterThanOne)(
+        populationCount(_) shouldBe odd
+      )
+    }
+
+    "must return the correct result for hand-picked acceptance criteria" in {
+      populationCount(8) shouldBe 1
+      populationCount(15) shouldBe 4
+      populationCount(19) shouldBe 3
+    }
+
+    "must return the same result as the control function" in {
+      /*
+        Scala has a toBinaryString which could be used to realise this requirement.
+        As that doesn't seem particularly in the spirit of the exercise I've used it
+        as a control function to prove my own implementation.
+       */
+      val controlFunc: Int => Int = _.toBinaryString.count(_ == '1')
+      GeneratorDrivenPropertyChecks.forAll(numbersGreaterThanZero)(n =>
+        populationCount(n) shouldBe controlFunc(n)
+      )
     }
   }
+}
 
+object ExerciseSpec {
+  val isOdd: Int => Boolean = _ % 2 == 1
+
+  class OddMatcher extends BeMatcher[Int] {
+    def apply(left: Int) =
+      MatchResult(
+        isOdd(left),
+        left.toString + " was even",
+        left.toString + " was odd"
+      )
+  }
+
+  val odd = new OddMatcher
 }
